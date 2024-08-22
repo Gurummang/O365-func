122833cc26c2245bf33d631e433a4c2fa4f3fd5b
2W <hsp003636@gmail.com>
2024-08-22 12:03:22 +0900

    init

---
+++ src/main/java/com/GASB/o365_func/O365FuncApplication.java
@@ -0,0 +1,13 @@
+package com.GASB.o365_func;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class O365FuncApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(O365FuncApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/GASB/o365_func/O365FuncApplicationTests.java
@@ -0,0 +1,13 @@
+package com.GASB.o365_func;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class O365FuncApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
