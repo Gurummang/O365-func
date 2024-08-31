e00d73e636394fa38865ce93fe7485b2bca7bfc2
2W <hsp003636@gmail.com>
2024-08-31 16:36:50 +0900

    KAN-442 <add : O365초기화 큐 리시버 추가>

---
+++ src/main/java/com/GASB/o365_func/service/MessageReceiver.java
@@ -0,0 +1,26 @@
+package com.GASB.o365_func.service;
+
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.amqp.rabbit.annotation.RabbitListener;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Component;
+
+@Component
+@Slf4j
+public class MessageReceiver {
+    private final MsInitService msInitService;
+
+    @Autowired
+    public MessageReceiver(MsInitService msInitService) {
+        this.msInitService = msInitService;
+    }
+    @RabbitListener(queues = "${rabbitmq.O365_INIT_QUEUE}") //수정 필요
+    public void receiveMessage(int message) {
+        try {
+            log.info("Received message from queue: " + message);
+            msInitService.fetchAndSaveAll(message);
+        } catch (Exception e) {
+            log.error("An error occurred while processing the message: {}", e.getMessage(), e);
+        }
+    }
+}
