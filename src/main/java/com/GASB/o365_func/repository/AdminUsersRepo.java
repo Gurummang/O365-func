d8b2649f5675357544b791e31fa9a7a9337f9ea1
2W <hsp003636@gmail.com>
2024-09-02 09:57:31 +0900

    DEV <add : org_id조회 쿼리문 수정>

--- src/main/java/com/GASB/o365_func/repository/AdminUsersRepo.java
+++ src/main/java/com/GASB/o365_func/repository/AdminUsersRepo.java
@@ -3,12 +3,16 @@ package com.GASB.o365_func.repository;
 
 import com.GASB.o365_func.model.entity.AdminUsers;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
+import org.springframework.data.repository.query.Param;
 import org.springframework.stereotype.Repository;
 
 import java.util.Optional;
 
 @Repository
 public interface AdminUsersRepo extends JpaRepository<AdminUsers, Integer> {
-    Optional<AdminUsers> findByEmail(String email);
+
+    @Query("SELECT a.org.id FROM AdminUsers a where a.email = :email")
+    int findByEmail(@Param("email")String email);
     boolean existsByEmail(String email);
 }
