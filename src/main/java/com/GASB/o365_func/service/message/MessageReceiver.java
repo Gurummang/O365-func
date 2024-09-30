6ee034f43db4aacbf44b252908445587d1a37f92
2W <hsp003636@gmail.com>
2024-10-01 00:02:10 +0900

    DEV <fix : 삭제 요청 리시버 추가 >

--- src/main/java/com/GASB/o365_func/service/message/MessageReceiver.java
+++ src/main/java/com/GASB/o365_func/service/message/MessageReceiver.java
@@ -1,5 +1,7 @@
 package com.GASB.o365_func.service.message;
 
+import com.GASB.o365_func.config.RabbitMQProperties;
+import com.GASB.o365_func.service.MsFileService;
 import com.GASB.o365_func.service.MsInitService;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.amqp.rabbit.annotation.RabbitListener;
@@ -7,14 +9,18 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.dao.DataAccessException;
 import org.springframework.stereotype.Component;
 
+import java.util.List;
+import java.util.Map;
+
 @Component
 @Slf4j
 public class MessageReceiver {
     private final MsInitService msInitService;
-
+    private final MsFileService msFileService;
     @Autowired
-    public MessageReceiver(MsInitService msInitService) {
+    public MessageReceiver(MsInitService msInitService, MsFileService msFileService) {
         this.msInitService = msInitService;
+        this.msFileService = msFileService;
     }
     @RabbitListener(queues = "${rabbitmq.O365_INIT_QUEUE}")
     public void receiveMessage(int message) {
@@ -32,5 +38,20 @@ public class MessageReceiver {
             log.error("An unexpected error occurred while processing the message: {}", e.getMessage(), e);
         }
     }
+
+
+    @RabbitListener(queues = "{rabbitmq.O365_DELETE_QUEUE}")
+    public void receiveDeleteMessage(List<Map<String,String>> message){
+        try {
+            log.info("Received message from queue: " + message);
+            msFileService.fileDelete(message);
+        } catch (DataAccessException e) {
+            log.error("Error deleting data: {}", e.getMessage(), e);
+        } catch (Exception e) {
+            log.error("An unexpected error occurred while processing the message: {}", e.getMessage(), e);
+        }
+    }
+
+
 
 }
